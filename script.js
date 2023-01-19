cdb9d9a2bf2d7797d43e38e823d5257e9e71e44a
Marsel Sattarov <fsc-3@tages.ru>
2023-01-19 16:33:19 +0500

    add leftHandler

--- script.js
+++ script.js
@@ -5,24 +5,40 @@ let rightPosition = 0
 let imgBlockPosition = 0
 // FUNCTIONS
 const rightHandler = () => {
-  rightPosition += 1
-  imgBlockPosition += 1
-  if (rightPosition > 5) {
-   rightPosition = 0
-  }
-  heroImg.style.left = `-${rightPosition * 288}`
-  imgBlock.style.left = `${imgBlockPosition * 20}`
+   heroImg.style.transform = 'scale(-1, 1)'
+   rightPosition += 1
+   imgBlockPosition += 1
+   if (rightPosition > 5) {
+      rightPosition = 0
+   }
+   heroImg.style.left = `-${rightPosition * 288}`
+   imgBlock.style.left = `${imgBlockPosition * 20}`
+}
+const leftHandler = () => {
+
+   heroImg.style.transform = 'scale(1, 1)'
+   rightPosition += 1
+   imgBlockPosition -= 1
+   if (rightPosition > 5) {
+      rightPosition = 0
+   }
+   heroImg.style.left = `-${rightPosition * 288}`
+   imgBlock.style.left = `${imgBlockPosition * 20}`
 }
 // EVENT HANDLERS
 
 let timer = null
-
+let x = 0
+let halfWidth = window.screen.width / 2
 let onTouchStart = (event) => {
+   event.preventDefault()
+   x = (event.type === 'mousedown') ? x = event.screenX : event.touches[0].screenX
    timer = setInterval(() => {
-      rightHandler()
+      (x > halfWidth) ? rightHandler() : leftHandler();
    }, 130)
 }
 let onTouchEnd = (event) => {
+   event.preventDefault()
    clearInterval(timer)
 }
 
@@ -30,3 +46,4 @@ window.onmousedown = onTouchStart
 window.ontouchstart = onTouchStart
 
 window.onmouseup = onTouchEnd
+window.ontouchend = onTouchEnd
